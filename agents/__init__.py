64fafcf47cbf72a536c304a4bfef5a3ac7df1189
Lesterpaintstheworld <reynolds.nicorr@gmail.com>
2024-11-10 09:16:30 +0100

    refactor: Simplify agent class discovery by removing explicit module import

--- agents/__init__.py
+++ agents/__init__.py
@@ -5,9 +5,8 @@ from aider_agent import AiderAgent
 
 # Discover and import all agent classes dynamically
 agent_classes = {}
-agent_types_module = importlib.import_module('.agent_types', package=__package__)
 
-for name, obj in inspect.getmembers(agent_types_module):
+for name, obj in inspect.getmembers():
     if (inspect.isclass(obj) and 
         issubclass(obj, AiderAgent) and 
         obj != AiderAgent):
